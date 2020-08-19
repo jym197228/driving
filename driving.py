96297cf018bddc0457d5cf9b72313534a32d8a28
Chris Chung <jym197@gmail.com>
2020-08-19 15:47:46 +0800

    新增美國及錯誤訊息

--- driving.py
+++ driving.py
@@ -6,5 +6,10 @@ if country == '台灣':
 		print('您可以開車')
 	else:
 		print('您還不能開車')
+elif country == '美國':
+	if age >= 16:
+		print('您可以開車')
+	else:
+		print('您還不能開車')
 else:
-	print('您的國家不再我們的判別範圍之內。')
+	print('您的國家目前還不再我們的判別範圍之內，目前只能判別"台灣"以及"美國"')
